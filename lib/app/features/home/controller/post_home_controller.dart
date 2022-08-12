2d03fd728b266fb80e552cb90fd4056b552687f9
minzinphyo <minzinphyo143@gmail.com>
2022-08-12 22:28:30 +0630

    - Bugs Fixed

--- lib/app/features/home/controller/post_home_controller.dart
+++ lib/app/features/home/controller/post_home_controller.dart
@@ -20,6 +20,7 @@ class PostHomeController extends BaseController {
   Rx<TextEditingController> searchTextEditingController =
       TextEditingController().obs;
   RxBool isSearch = false.obs;
+  RxBool isFavourite = false.obs;
 
   List<PostData> get postList => _postList;
 
@@ -119,67 +120,7 @@ class PostHomeController extends BaseController {
     Get.offAllNamed(Paths.LOGIN);
   }
 
-  //GetX pagination
-  // We will fetch data from this Rest api
-  final _baseUrl = 'https://jsonplaceholder.typicode.com/posts';
-
-  // At the beginning, we fetch the first 20 posts
-  RxInt page = 0.obs;
-  RxInt limit = 10.obs;
-
-  // There is next page or not
-  RxBool hasNextPage = true.obs;
-
-  // Used to display loading indicators when _firstLoad function is running
-  RxBool isFirstLoadRunning = false.obs;
-
-  // Used to display loading indicators when _loadMore function is running
-  RxBool isLoadMoreRunning = false.obs;
-
-  // This holds the posts fetched from the server
-  RxList posts = [].obs;
-
-  // The controller for the ListView
-  final Rx<ScrollController> scrollController = ScrollController().obs;
-
-  // This function will be called when the app launches (see the initState function)
-  void firstLoad() async {
-    isFirstLoadRunning.value = true;
-    try {
-      final res = await http.get(
-          Uri.parse("$_baseUrl?_page=${page.value}&_limit=${limit.value}"));
-      posts.value = json.decode(res.body);
-      print("Post is ${posts.length}");
-    } catch (err) {
-      print('Something went wrong');
-    }
-    isFirstLoadRunning.value = false;
-  }
-
-  void loadMore() async {
-    if (hasNextPage.value == true &&
-        isFirstLoadRunning.value == false &&
-        isLoadMoreRunning.value == false &&
-        scrollController.value.position.extentAfter < 300) {
-      isLoadMoreRunning.value =
-          true; // Display a progress indicator at the bottom
-      page.value += 1; // Increase _page by 1
-      try {
-        final res = await http.get(
-            Uri.parse("$_baseUrl?_page=${page.value}&_limit=${limit.value}"));
-
-        final List fetchedPosts = json.decode(res.body);
-        if (fetchedPosts.isNotEmpty) {
-          posts.addAll(fetchedPosts);
-        } else {
-          // This means there is no more data
-          // and therefore, we will not send another GET request
-          hasNextPage.value = false;
-        }
-      } catch (err) {
-        print('Something went wrong!');
-      }
-      isLoadMoreRunning.value = false;
-    }
+  void savePost(bool isSave){
+    isFavourite.value = !isSave;
   }
 }
--- lib/app/widget/post_item_widget.dart
+++ lib/app/widget/post_item_widget.dart
@@ -1,21 +1,34 @@
 import 'package:blog_post_flutter/app/constant/routing/app_routes.dart';
 import 'package:blog_post_flutter/app/core/utils/date_utils.dart';
 import 'package:blog_post_flutter/app/core/utils/dialog_utils.dart';
+import 'package:blog_post_flutter/app/core/utils/global_key_utils.dart';
 import 'package:blog_post_flutter/app/data/model/post/post_ob.dart';
+import 'package:blog_post_flutter/app/features/home/controller/post_home_controller.dart';
 import 'package:blog_post_flutter/app/widget/cached_network_image_widget.dart';
 import 'package:blog_post_flutter/app/widget/text_view_widget.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class PostItemWidget extends StatelessWidget {
+class PostItemWidget extends StatefulWidget {
   final PostData postData;
 
-  const PostItemWidget({
+  PostItemWidget({
     Key? key,
     required this.postData,
   }) : super(key: key);
 
+  @override
+  State<PostItemWidget> createState() => _PostItemWidgetState();
+}
+
+class _PostItemWidgetState extends State<PostItemWidget> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return (Column(
@@ -30,13 +43,13 @@ class PostItemWidget extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   TextViewWidget(
-                    postData.title ?? "",
+                    widget.postData.title ?? "",
                     textSize: 18,
                     fontWeight: FontWeight.w600,
                   ),
                   const SizedBox(height: 5),
                   TextViewWidget(
-                    postData.content ?? "",
+                    widget.postData.content ?? "",
                     textSize: 15,
                     lineHeight: 1.3,
                     textAlign: TextAlign.justify,
@@ -44,7 +57,7 @@ class PostItemWidget extends StatelessWidget {
                   ),
                   InkWell(
                     onTap: () {
-                      Get.toNamed(Paths.POST_DETAIL, arguments: postData.id);
+                      Get.toNamed(Paths.POST_DETAIL, arguments: widget.postData.id);
                     },
                     child: const TextViewWidget(
                       "See More",
@@ -58,14 +71,14 @@ class PostItemWidget extends StatelessWidget {
               ),
             ),
             const SizedBox(width: 20),
-            if (postData.image != null)
+            if (widget.postData.image != null)
               InkWell(
                 onTap: () => DialogUtils.showPreviewImageDialog(
                   context,
-                  postData.image,
+                  widget.postData.image,
                 ),
                 child: CachedNetworkImageWidget(
-                  imageUrl: postData.image,
+                  imageUrl: widget.postData.image,
                   width: 72,
                   height: 72,
                 ),
@@ -75,23 +88,23 @@ class PostItemWidget extends StatelessWidget {
         const SizedBox(height: 5),
         Row(
           children: [
-            postData.owner!.profilePicture != null
+            widget.postData.owner!.profilePicture != null
                 ? InkWell(
                     onTap: () => DialogUtils.showPreviewImageDialog(
-                        context, postData.owner!.profilePicture),
+                        context, widget.postData.owner!.profilePicture),
                     child: Container(
                       width: 25,
                       height: 25,
                       decoration: BoxDecoration(border: Border.all(width: 2)),
                       child: CachedNetworkImageWidget(
-                        imageUrl: postData.owner!.profilePicture,
+                        imageUrl: widget.postData.owner!.profilePicture,
                         width: 25,
                         height: 25,
                       ),
                     ),
                   )
                 : const SizedBox(),
-            postData.owner!.profilePicture != null
+            widget.postData.owner!.profilePicture != null
                 ? const SizedBox(width: 10)
                 : const SizedBox(),
             Expanded(
@@ -106,31 +119,35 @@ class PostItemWidget extends StatelessWidget {
                   children: <TextSpan>[
                     TextSpan(
                         text:
-                            "${postData.owner!.firstName} ${postData.owner!.lastName}"),
-                    if (postData.owner!.university != null)
+                            "${widget.postData.owner!.firstName} ${widget.postData.owner!.lastName}"),
+                    if (widget.postData.owner!.university != null)
                       const TextSpan(
                           text: ' in ',
                           style: TextStyle(color: Color(0XFFA9A9A9))),
-                    TextSpan(text: postData.owner!.university),
+                    TextSpan(text: widget.postData.owner!.university),
                   ],
                 ),
               ),
             ),
-            IconButton(
-              icon: Icon(Icons.bookmark_border,
-                  size: 28,
-                  color: postData.isSaved == true
-                      ? Colors.red
-                      : Color(0xffA9A9A9)),
-              onPressed: () {
-                print("Toggle Save");
-              },
-            ),
+            // Obx(() => IconButton(
+            //   icon: Icon(Icons.bookmark_border,
+            //       size: 28,
+            //       color: postHomeController.isFavourite.value == true
+            //           ? Colors.red
+            //           : Color(0xffA9A9A9)),
+            //   onPressed: () {
+            //     if(GlobalVariable.token == null){
+            //       Get.offAllNamed(Paths.MAIN_HOME,arguments: 4);
+            //     }else{
+            //       postHomeController.savePost(postHomeController.isFavourite.value);
+            //     }
+            //   },
+            // ),
           ],
         ),
-        if (postData.createdDate != null)
+        if (widget.postData.createdDate != null)
           TextViewWidget(
-            DateUtil.convertDateFormat(postData.createdDate!, DAY_MONTH_YEAR),
+            DateUtil.convertDateFormat(widget.postData.createdDate!, DAY_MONTH_YEAR),
             textColor: Color(0xffA9A9A9),
             textSize: 12,
           )
